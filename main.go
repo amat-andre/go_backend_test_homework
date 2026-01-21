65fcaf29dd1217e6993811414164e759a5dd9f3b
Alexander Matveenkov <amat.andre@gmail.com>
2026-01-21 23:54:20 +0300

    Fix

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func Add(a, b int) int {
 }
 
 func main() {
+<<<<<<< HEAD
+	fmt.Println("Я люблю домашку. А это нужно reset'ить")
+=======
 	fmt.Println("Я люблю домашку")
+>>>>>>> refs/remotes/origin/main
 }
 // Я в шаке от этих ваших программирований 
